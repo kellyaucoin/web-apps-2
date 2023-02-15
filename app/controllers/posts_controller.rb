9f6c51ae4aefcbd0e854b808e62d2dd2d64b5ed6
kellyaucoin <kelly.aucoin@kellogg.northwestern.edu>
2023-02-15 06:32:34 +0000

    tacos done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,18 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @posts = Post.new
+    @posts["author"] = params["post"]["author"]
+    @posts["body"] = params["post"]["body"]
+    @posts["image"] = params["post"]["image"]
+    @posts.save
+    redirect_to "/posts"
+  end
 
 end
